8d27d2ffb50042ceaeda9eb08a439e45d1872dcd
Tomás Messina <45271713+messina8@users.noreply.github.com>
2022-11-03 08:53:24 -0300

    Started GUI

---
+++ GUI.py
@@ -0,0 +1,87 @@
+import tkinter
+import tkinter.messagebox
+import customtkinter
+
+
+class App(customtkinter.CTk):
+    WIDTH = 780
+    HEIGHT = 520
+
+    def __init__(self):
+        super().__init__()
+
+        self.title('Melenium')
+        self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+
+        # configure grid layout (2x1)
+        self.grid_columnconfigure(1, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        self.frame_left = customtkinter.CTkFrame(master=self,
+                                                 width=180,
+                                                 corner_radius=0)
+        self.frame_left.grid(row=0, column=0, sticky="nswe")
+
+        self.frame_right = customtkinter.CTkFrame(master=self)
+        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+
+        # ============ frame_left ============
+
+        # configure grid layout (1x11)
+        self.frame_left.grid_rowconfigure(0, minsize=10)  # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
+        self.frame_left.grid_rowconfigure(8, minsize=20)  # empty row with minsize as spacing
+        self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
+
+        self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
+                                              text="CustomTkinter",
+                                              text_font=("Roboto Medium", -16))  # font name and size in px
+        self.label_1.grid(row=1, column=0, pady=10, padx=10)
+
+        self.button_1 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="CTkButton",
+                                                command=self.button_event)
+        self.button_1.grid(row=2, column=0, pady=10, padx=20)
+
+        self.button_2 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="CTkButton",
+                                                command=self.button_event)
+        self.button_2.grid(row=3, column=0, pady=10, padx=20)
+
+        self.button_3 = customtkinter.CTkButton(master=self.frame_left,
+                                                text="CTkButton",
+                                                command=self.button_event)
+        self.button_3.grid(row=4, column=0, pady=10, padx=20)
+
+        self.label_mode = customtkinter.CTkLabel(master=self.frame_left, text="Appearance Mode:")
+        self.label_mode.grid(row=9, column=0, pady=0, padx=20, sticky="w")
+
+        self.option_menu_1 = customtkinter.CTkOptionMenu(master=self.frame_left,
+                                                         values=["Light", "Dark", "System"],
+                                                         command=self.change_appearance_mode)
+        self.option_menu_1.grid(row=10, column=0, pady=10, padx=20, sticky="w")
+
+        # ============ frame_right ============
+
+        # configure grid layout (3x7)
+        self.frame_right.rowconfigure((0, 1, 2, 3), weight=1)
+        self.frame_right.rowconfigure(7, weight=10)
+        self.frame_right.columnconfigure((0, 1), weight=1)
+        self.frame_right.columnconfigure(2, weight=0)
+
+        self.frame_info = customtkinter.CTkFrame(master=self.frame_right)
+        self.frame_info.grid(row=0, column=0, columnspan=2, rowspan=4, pady=20, padx=20, sticky="nsew")
+
+    def button_event(self):
+        print("Button pressed")
+
+    def change_appearance_mode(self, new_appearance_mode):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def on_closing(self, event=0):
+        self.destroy()
+
+
+app = App()
+app.mainloop()

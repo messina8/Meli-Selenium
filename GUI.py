e5057d8a75f802d1722b34e67dbbd0d16f84b208
Tomás Messina <45271713+messina8@users.noreply.github.com>
2022-11-04 08:55:18 -0300

    Progress on GUI

--- GUI.py
+++ GUI.py
@@ -1,11 +1,11 @@
 import tkinter
 import tkinter.messagebox
-import customtkinter
+import customtkinter as ctki
 
 
-class App(customtkinter.CTk):
-    WIDTH = 780
-    HEIGHT = 520
+class App(ctki.CTk):
+    WIDTH = 600
+    HEIGHT = 720
 
     def __init__(self):
         super().__init__()
@@ -14,70 +14,38 @@ class App(customtkinter.CTk):
         self.geometry(f"{App.WIDTH}x{App.HEIGHT}")
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
-        # configure grid layout (2x1)
-        self.grid_columnconfigure(1, weight=1)
         self.grid_rowconfigure(0, weight=1)
+        self.grid_columnconfigure(0, weight=1)
 
-        self.frame_left = customtkinter.CTkFrame(master=self,
-                                                 width=180,
-                                                 corner_radius=0)
-        self.frame_left.grid(row=0, column=0, sticky="nswe")
+        self.textbox = ctki.CTkTextbox(master=self)
+        self.textbox.grid(row=0, column=0, columnspan=3, padx=20, pady=(20, 0), sticky="nsew")
 
-        self.frame_right = customtkinter.CTkFrame(master=self)
-        self.frame_right.grid(row=0, column=1, sticky="nswe", padx=20, pady=20)
+        self.tech_check = ctki.CTkCheckBox(master=self, text="Fill Tech", onvalue="on", offvalue="off")
+        self.tech_check.grid(row=1, column=0, padx=20, pady=20, sticky='')
 
-        # ============ frame_left ============
+        self.price_check = ctki.CTkCheckBox(master=self, text="Change Price", onvalue="on", offvalue="off")
+        self.price_check.grid(row=1, column=1, padx=20, pady=20, sticky='')
 
-        # configure grid layout (1x11)
-        self.frame_left.grid_rowconfigure(0, minsize=10)  # empty row with minsize as spacing
-        self.frame_left.grid_rowconfigure(5, weight=1)  # empty row as spacing
-        self.frame_left.grid_rowconfigure(8, minsize=20)  # empty row with minsize as spacing
-        self.frame_left.grid_rowconfigure(11, minsize=10)  # empty row with minsize as spacing
+        self.change_stock = ctki.CTkCheckBox(master=self, text="Change Stock", onvalue="on", offvalue="off")
+        self.change_stock.grid(row=1, column=2, padx=20, pady=20, sticky='')
 
-        self.label_1 = customtkinter.CTkLabel(master=self.frame_left,
-                                              text="CustomTkinter",
-                                              text_font=("Roboto Medium", -16))  # font name and size in px
-        self.label_1.grid(row=1, column=0, pady=10, padx=10)
+        self.new_price = ctki.CTkEntry(master=self, placeholder_text='Enter new price')
+        self.new_price.grid(row=2, column=1, padx=20, pady=20, sticky='ew')
 
-        self.button_1 = customtkinter.CTkButton(master=self.frame_left,
-                                                text="CTkButton",
-                                                command=self.button_event)
-        self.button_1.grid(row=2, column=0, pady=10, padx=20)
+        self.stock_change = ctki.CTkEntry(master=self, placeholder_text='Enter stock change')
+        self.stock_change.grid(row=2, column=2, padx=20, pady=20, sticky='ew')
 
-        self.button_2 = customtkinter.CTkButton(master=self.frame_left,
-                                                text="CTkButton",
-                                                command=self.button_event)
-        self.button_2.grid(row=3, column=0, pady=10, padx=20)
+        self.publication = ctki.CTkEntry(master=self, placeholder_text='Enter title or publi ID')
+        self.publication.grid(row=3, column=0, columnspan=3, padx=20, pady=20, sticky='ew')
 
-        self.button_3 = customtkinter.CTkButton(master=self.frame_left,
-                                                text="CTkButton",
-                                                command=self.button_event)
-        self.button_3.grid(row=4, column=0, pady=10, padx=20)
+        self.submit = ctki.CTkButton(master=self, text='Go')
+        self.submit.grid(row=4, column=1, columnspan=2, padx=20, pady=20, sticky='ew')
 
-        self.label_mode = customtkinter.CTkLabel(master=self.frame_left, text="Appearance Mode:")
-        self.label_mode.grid(row=9, column=0, pady=0, padx=20, sticky="w")
-
-        self.option_menu_1 = customtkinter.CTkOptionMenu(master=self.frame_left,
-                                                         values=["Light", "Dark", "System"],
-                                                         command=self.change_appearance_mode)
-        self.option_menu_1.grid(row=10, column=0, pady=10, padx=20, sticky="w")
-
-        # ============ frame_right ============
-
-        # configure grid layout (3x7)
-        self.frame_right.rowconfigure((0, 1, 2, 3), weight=1)
-        self.frame_right.rowconfigure(7, weight=10)
-        self.frame_right.columnconfigure((0, 1), weight=1)
-        self.frame_right.columnconfigure(2, weight=0)
-
-        self.frame_info = customtkinter.CTkFrame(master=self.frame_right)
-        self.frame_info.grid(row=0, column=0, columnspan=2, rowspan=4, pady=20, padx=20, sticky="nsew")
+        self.mass_edit = ctki.CTkButton(master=self, text='Mass edit')
+        self.mass_edit.grid(row=4, column=0, padx=20, pady=20, sticky='ew')
 
     def button_event(self):
-        print("Button pressed")
-
-    def change_appearance_mode(self, new_appearance_mode):
-        customtkinter.set_appearance_mode(new_appearance_mode)
+        pass
 
     def on_closing(self, event=0):
         self.destroy()
